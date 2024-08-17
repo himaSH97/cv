09cbca64fdf07705ac32a8c141f3e08a770e965f
himash <nipun946@gmail.com>
2024-08-17 15:53:09 +0530

    hopeful fix

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,9 @@ const Home = lazy(() => import('./pages/home'));
 import { pdfjs } from "react-pdf";
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  "pdfjs-dist/build/pdf.worker.min.mjs",
+  "/node_modules/pdfjs-dist/build/pdf.worker.min.mjs",
   import.meta.url
 ).toString();
-
-
 
 function App() {
 
