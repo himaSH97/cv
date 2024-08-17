eb545439da7500658d07269289e1d44190251e9a
himash <nipun946@gmail.com>
2024-08-17 18:26:50 +0530

    added lagacy worker v3

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,20 @@ const Home = lazy(() => import('./pages/home'));
 
 import { pdfjs } from "react-pdf";
 
+// @ts-expect-error This does not exist outside of polyfill which this is doing
+if (typeof Promise.withResolvers === 'undefined') {
+  if (window)
+      // @ts-expect-error This does not exist outside of polyfill which this is doing
+      window.Promise.withResolvers = function () {
+          let resolve, reject;
+          const promise = new Promise((res, rej) => {
+              resolve = res;
+              reject = rej;
+          });
+          return { promise, resolve, reject };
+      };
+}
+
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   "/node_modules/pdfjs-dist/legacy/build/pdf.worker.min.mjs",
   import.meta.url
